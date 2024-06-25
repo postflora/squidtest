efa41857704b36e61e7cbb7937eff369a339070b
postflora <116581158+postflora@users.noreply.github.com>
2024-06-25 16:40:58 +0200

    Update capture_screenshot.py

--- .github/workflows/capture_screenshot.py
+++ .github/workflows/capture_screenshot.py
@@ -1,27 +1,38 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
-import sys
 
-def capture_screenshot(index):
+def capture_screenshot(proxy_ip, proxy_port, username, password):
     options = Options()
     options.headless = True
 
     profile = webdriver.FirefoxProfile()
     profile.set_preference("network.proxy.type", 1)
-    profile.set_preference("network.proxy.http", f"172.18.0.{index + 1}")
-    profile.set_preference("network.proxy.http_port", 3128)
-    profile.set_preference("network.proxy.ssl", f"172.18.0.{index + 1}")
-    profile.set_preference("network.proxy.ssl_port", 3128)
+    profile.set_preference("network.proxy.http", proxy_ip)
+    profile.set_preference("network.proxy.http_port", proxy_port)
+    profile.set_preference("network.proxy.ssl", proxy_ip)
+    profile.set_preference("network.proxy.ssl_port", proxy_port)
+    profile.set_preference("network.proxy.autoconfig_url", "")
+    profile.set_preference("network.proxy.no_proxies_on", "localhost, 127.0.0.1")
+
+    # Set proxy authentication credentials
+    profile.set_preference("network.proxy.username", username)
+    profile.set_preference("network.proxy.password", password)
+
     profile.update_preferences()
 
     driver = webdriver.Firefox(options=options, firefox_profile=profile)
     driver.get("https://whatismyipaddress.com/")
     time.sleep(5)  # Allow time for the page to load
 
-    driver.save_screenshot(f"/home/ubuntu/screenshots/screenshot_{index}.png")
+    driver.save_screenshot(f"/home/ubuntu/screenshots/screenshot_{proxy_ip.split('.')[-1]}.png")
     driver.quit()
 
 if __name__ == "__main__":
-    index = int(sys.argv[1])
-    capture_screenshot(index)
+    proxy_ip = f"172.18.0.{sys.argv[1]}"
+    proxy_port = 3128
+    username = "flash"
+    password = "flash"
+
+    capture_screenshot(proxy_ip, proxy_port, username, password)
